d11c5c7b847b9d341b882093894603c20cb6b919
smokehill <valera.padolochniy@gmail.com>
2020-05-31 12:53:26 +0300

    install fix

--- ubuntu_16.04/install.sh
+++ ubuntu_16.04/install.sh
@@ -11,9 +11,11 @@ docker build -t node:10.x ./node_10.x/
 # php: 5.6, 7.2, 7.4
 docker build -t php:5.6 ./php_5.6/
 docker build -t php:7.2 ./php_7.2/
+docker build -t php:7.2-oracle-oci8 ./php_7.2_oracle_oci8/
 docker build -t php:7.4 ./php_7.4/
 # mysql: 5.5, 5.7, 8.0
 docker build -t mysql:5.5 ./mysql_5.5/
+docker build -t mysql:5.5-json-udfs ./mysql_5.5_json_udfs/
 docker build -t mysql:5.7 ./mysql_5.7/
 docker build -t mysql:8.0 ./mysql_8.0/
 # sphinx: 2.2
