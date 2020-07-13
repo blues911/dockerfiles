2a9fad39748ef168f9221ad329e8260c6a338334
smokehill <valera.padolochniy@gmail.com>
2020-07-13 16:57:29 +0300

    postgresql_9.5 pgdata fix

--- ubuntu_16.04/postgresql_9.5/entrypoint.sh
+++ ubuntu_16.04/postgresql_9.5/entrypoint.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-if [ ! -d /var/lib/postgresql/data/base ]; then
+if [ ! -d "/var/lib/postgresql/9.5/data/base" ]; then
     echo "[0] initialize databases"
-    mkdir -p /var/lib/postgresql/data
-    chown -R postgres:postgres /var/lib/postgresql/data
-    su - postgres -c '/usr/lib/postgresql/9.5/bin/initdb --pgdata=/var/lib/postgresql/data'
+    mkdir -p /var/lib/postgresql/9.5/data
+    chown -R postgres:postgres /var/lib/postgresql/9.5/data
+    su - postgres -c "/usr/lib/postgresql/9.5/bin/initdb --pgdata=/var/lib/postgresql/9.5/data"
 fi
 
-su - postgres -c '/usr/lib/postgresql/9.5/bin/postgres -D /var/lib/postgresql/data'
+# change postgres password
+# su postgres -c "psql -c \"ALTER USER postgres WITH PASSWORD 'postgres';\""
+su - postgres -c "/usr/lib/postgresql/9.5/bin/postgres -c config_file=/etc/postgresql/9.5/main/postgresql.conf"
